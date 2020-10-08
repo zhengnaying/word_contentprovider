419c975b4cedfe51717bd4899376f88b459285a3
zhengnaying <55484284+zhengnaying@users.noreply.github.com>
2020-10-08 19:09:01 +0800

    1

--- app/src/main/java/com/example/word_contentprovider/MainActivity.java
+++ app/src/main/java/com/example/word_contentprovider/MainActivity.java
@@ -90,6 +90,39 @@ public class MainActivity extends AppCompatActivity {
                 String strWord ="Banana";
                 String strMeaning ="banana";
                 String strSample = "This banana is very nice.";
+                ContentValues values = new ContentValues();
+
+                values.put(Words.Word.COLUMN_NAME_WORD, strSample);
+                values.put(Words.Word.COLUMN_NAME_MEANING,strMeaning);
+                values.put(Words.Word.COLUMN_NAME_SAMPLE,strSample);
+
+                Uri uri = Uri.parse(Words.Word.CONTENT_URI_STRING + "/"+id);
+                int result = resolver.update(uri,values,null,null);
+            }
+        });
+
+        buttonSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String id ="3";
+                Uri uri = Uri.parse(Words.Word.CONTENT_URI_STRING +"/"+id);
+                Cursor cursor = resolver.query(Words.Word.CONTENT_URI,new String[]{Words.Word._ID,
+                        Words.Word.COLUMN_NAME_WORD, Words.Word.COLUMN_NAME_MEANING, Words.Word.COLUMN_NAME_SAMPLE},null,null,null);
+                if(cursor == null){
+                    Toast.makeText(MainActivity.this,"没有找到数据",Toast.LENGTH_LONG).show();
+                    return;
+                }
+                String msg="";
+                if(cursor.moveToNext()){
+                    do{
+                        msg += "ID : " + String.valueOf(cursor.getString(cursor.getColumnIndex(Words.Word._ID))) +",";
+                        msg += "单词:  " +cursor.getString(cursor.getColumnIndex(Words.Word.COLUMN_NAME_WORD))+",";
+                        msg += "含义: " +cursor.getString(cursor.getColumnIndex(Words.Word.COLUMN_NAME_MEANING))+",";
+                        msg+= "例句: "+ cursor.getString(cursor.getColumnIndex(Words.Word.COLUMN_NAME_SAMPLE))+"\n";
+
+                    }while (cursor.moveToNext());
+                }
+                Log.v(TAG,msg);
             }
         });
     }
