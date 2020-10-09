23a601ad95040b2d1a839ebad24467a311bf066a
zhengnaying <55484284+zhengnaying@users.noreply.github.com>
2020-10-09 09:06:00 +0800

    1

--- app/src/main/java/com/example/word_contentprovider/MainActivity.java
+++ app/src/main/java/com/example/word_contentprovider/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
                 String msg="";
                 if(c.moveToNext()){
                     do{
-                        msg += "ID : " + String.valueOf(c.getString(c.getColumnIndex(Words.Word._ID))) +",";
+                        msg += "ID : " + c.getString(c.getColumnIndex(Words.Word._ID)) +",";
                         msg += "单词:  " +c.getString(c.getColumnIndex(Words.Word.COLUMN_NAME_WORD))+",";
                         msg += "含义: " +c.getString(c.getColumnIndex(Words.Word.COLUMN_NAME_MEANING))+",";
                         msg+= "例句: "+ c.getString(c.getColumnIndex(Words.Word.COLUMN_NAME_SAMPLE))+"\n";
@@ -51,77 +51,79 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //增加
         buttonInsert.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                String strWord = "Banana";
-                String strMeaning = "banana";
-                String strSample = "This banana is very nice";
+            public void onClick(View view) {
+                String strWord="Banana";
+                String strMeaning="banana";
+                String strSample="This banana is very nice.";
                 ContentValues values = new ContentValues();
 
-                values.put(Words.Word.COLUMN_NAME_WORD,strWord);
-                values.put(Words.Word.COLUMN_NAME_MEANING,strMeaning);
-                values.put(Words.Word.COLUMN_NAME_SAMPLE,strSample);
+                values.put(Words.Word.COLUMN_NAME_WORD, strWord);
+                values.put(Words.Word.COLUMN_NAME_MEANING, strMeaning);
+                values.put(Words.Word.COLUMN_NAME_SAMPLE, strSample);
 
-                Uri newUri = resolver.insert(Words.Word.CONTENT_URI,values);
+                Uri newUri = resolver.insert(Words.Word.CONTENT_URI, values);
             }
         });
 
+        //删除
         buttonDelete.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                String id ="3";
-                Uri uri = Uri.parse(Words.Word.CONTENT_URI_STRING+"/"+id);
-                int result = resolver.delete(uri,null,null);
+            public void onClick(View view) {
+                String id="3";//简单起见，这里指定ID，用户可在程序中设置id的实际值
+                Uri uri = Uri.parse(Words.Word.CONTENT_URI_STRING);
+                int result = resolver.delete(uri, Words.Word._ID+"="+id, null);
             }
         });
 
-        buttonDeleteAll.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                resolver.delete(Words.Word.CONTENT_URI,null,null);
-            }
-        });
-
+        //修改
         buttonUpdate.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                String id ="3";
-                String strWord ="Banana";
-                String strMeaning ="banana";
-                String strSample = "This banana is very nice.";
+            public void onClick(View view) {
+                String id="3";
+                String strWord="Banana";
+                String strMeaning="香蕉";
+                String strSample="This banana is very nice.";
                 ContentValues values = new ContentValues();
 
-                values.put(Words.Word.COLUMN_NAME_WORD, strSample);
-                values.put(Words.Word.COLUMN_NAME_MEANING,strMeaning);
-                values.put(Words.Word.COLUMN_NAME_SAMPLE,strSample);
+                values.put(Words.Word.COLUMN_NAME_WORD, strWord);
+                values.put(Words.Word.COLUMN_NAME_MEANING, strMeaning);
+                values.put(Words.Word.COLUMN_NAME_SAMPLE, strSample);
 
-                Uri uri = Uri.parse(Words.Word.CONTENT_URI_STRING + "/"+id);
-                int result = resolver.update(uri,values,null,null);
+                Uri uri = Uri.parse(Words.Word.CONTENT_URI_STRING );
+                int result = resolver.update(uri, values, Words.Word._ID+"="+id, null);
+
             }
         });
 
+        //根据id查询
         buttonSearch.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                String id ="3";
-                Uri uri = Uri.parse(Words.Word.CONTENT_URI_STRING +"/"+id);
-                Cursor cursor = resolver.query(Words.Word.CONTENT_URI,new String[]{Words.Word._ID,
-                        Words.Word.COLUMN_NAME_WORD, Words.Word.COLUMN_NAME_MEANING, Words.Word.COLUMN_NAME_SAMPLE},null,null,null);
-                if(cursor == null){
-                    Toast.makeText(MainActivity.this,"没有找到数据",Toast.LENGTH_LONG).show();
+            public void onClick(View view) {
+
+                String id="3";
+                Cursor cursor = resolver.query(Words.Word.CONTENT_URI,
+                        null,
+                        Words.Word._ID+"="+id, null, null);
+                if (cursor == null){
+                    Toast.makeText(MainActivity.this,"没有找到记录",Toast.LENGTH_LONG).show();
                     return;
                 }
-                String msg="";
-                if(cursor.moveToNext()){
-                    do{
-                        msg += "ID : " + String.valueOf(cursor.getString(cursor.getColumnIndex(Words.Word._ID))) +",";
-                        msg += "单词:  " +cursor.getString(cursor.getColumnIndex(Words.Word.COLUMN_NAME_WORD))+",";
-                        msg += "含义: " +cursor.getString(cursor.getColumnIndex(Words.Word.COLUMN_NAME_MEANING))+",";
-                        msg+= "例句: "+ cursor.getString(cursor.getColumnIndex(Words.Word.COLUMN_NAME_SAMPLE))+"\n";
 
-                    }while (cursor.moveToNext());
+                //找到记录，这里简单起见，使用Log输出
+
+                String msg = "";
+                if (cursor.moveToFirst()){
+                    do{
+                        msg += "ID:" + cursor.getInt(cursor.getColumnIndex(Words.Word._ID)) + ",";
+                        msg += "单词：" + cursor.getString(cursor.getColumnIndex(Words.Word.COLUMN_NAME_WORD))+ ",";
+                        msg += "含义：" + cursor.getString(cursor.getColumnIndex(Words.Word.COLUMN_NAME_MEANING)) + ",";
+                        msg += "示例" + cursor.getString(cursor.getColumnIndex(Words.Word.COLUMN_NAME_SAMPLE)) + "\n";
+                    }while(cursor.moveToNext());
                 }
+
                 Log.v(TAG,msg);
             }
         });
--- app/src/main/java/com/example/word_contentprovider/Words.java
+++ app/src/main/java/com/example/word_contentprovider/Words.java
@@ -11,15 +11,7 @@ public class Words {
         public static final String COLUMN_NAME_MEANING = "meaning"; //字段： 单词含义
         public static final String COLUMN_NAME_SAMPLE = "sample"; //字段： 单词示例
 
-        //MIME类型
-        public static final String MIME_DIR_PREFIX = "vnd.android.cursor.dir";
-        public static final String MIME_ITEM_PREFIX = "vnd.android.cursor.item";
-        public static final String MINE_ITEM = "vnd.example.wordsbook.Words";
 
-        public static final String MINE_TYPE_SINGLE = MIME_ITEM_PREFIX + "/" + MINE_ITEM;
-        public static final String MINE_TYPE_MULTIPLE = MIME_DIR_PREFIX + "/" + MINE_ITEM;
-
-        public static final String PATH_SINGLE = "words/#";//单条数据的路径
         public static final String PATH_MULTIPLE = "words";//多条数据的路径
 
         //Content Uri
